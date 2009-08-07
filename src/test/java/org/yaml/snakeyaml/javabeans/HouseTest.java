bafcbf3a0468cc5163e0c66b4b4b9f91498ae481
somov <none@none>
2009-08-07 10:57:08 +0200

    no change: move test methods to show the best way first

--- src/test/java/org/yaml/snakeyaml/javabeans/HouseTest.java
+++ src/test/java/org/yaml/snakeyaml/javabeans/HouseTest.java
@@ -17,6 +17,42 @@ import org.yaml.snakeyaml.Util;
 
 public class HouseTest extends TestCase {
     /**
+     * no root global tag
+     */
+    public void testDump1() {
+        House house = new House();
+        FrontDoor frontDoor = new FrontDoor("qaz1", 5);
+        frontDoor.setKeytype("qwerty123");
+        house.setFrontDoor(frontDoor);
+        List<Room> rooms = new LinkedList<Room>();
+        rooms.add(new Room("Hall"));
+        rooms.add(new Room("Kitchen"));
+        house.setRooms(rooms);
+        Map<String, String> reminders = new TreeMap<String, String>();
+        reminders.put("today", "do nothig");
+        reminders.put("tomorrow", "go shoping");
+        house.setReminders(reminders);
+        house.setNumber(1);
+        house.setStreet("Wall Street");
+        JavaBeanDumper beanDumper = new JavaBeanDumper();
+        String yaml = beanDumper.dump(house);
+        String etalon = Util.getLocalResource("javabeans/house-dump1.yaml");
+        assertEquals(etalon, yaml);
+        // false is default
+        beanDumper = new JavaBeanDumper(false);
+        String output2 = beanDumper.dump(house);
+        assertEquals(etalon, output2);
+        // load
+        JavaBeanLoader<House> beanLoader = new JavaBeanLoader<House>(House.class);
+        House loadedHouse = beanLoader.load(yaml);
+        assertNotNull(loadedHouse);
+        assertEquals("Wall Street", loadedHouse.getStreet());
+        // dump again
+        String yaml3 = beanDumper.dump(loadedHouse);
+        assertEquals(yaml, yaml3);
+    }
+
+    /**
      * with global root class tag (global tag should be avoided)
      */
     public void testDump3() {
@@ -55,42 +91,6 @@ public class HouseTest extends TestCase {
         assertEquals(reminders, loadedHouse.getReminders());
         List<Room> loadedRooms = loadedHouse.getRooms();
         assertEquals(rooms, loadedRooms);
-        // dump again
-        String yaml3 = beanDumper.dump(loadedHouse);
-        assertEquals(yaml, yaml3);
-    }
-
-    /**
-     * no root global tag
-     */
-    public void testDump1() {
-        House house = new House();
-        FrontDoor frontDoor = new FrontDoor("qaz1", 5);
-        frontDoor.setKeytype("qwerty123");
-        house.setFrontDoor(frontDoor);
-        List<Room> rooms = new LinkedList<Room>();
-        rooms.add(new Room("Hall"));
-        rooms.add(new Room("Kitchen"));
-        house.setRooms(rooms);
-        Map<String, String> reminders = new TreeMap<String, String>();
-        reminders.put("today", "do nothig");
-        reminders.put("tomorrow", "go shoping");
-        house.setReminders(reminders);
-        house.setNumber(1);
-        house.setStreet("Wall Street");
-        JavaBeanDumper beanDumper = new JavaBeanDumper();
-        String yaml = beanDumper.dump(house);
-        String etalon = Util.getLocalResource("javabeans/house-dump1.yaml");
-        assertEquals(etalon, yaml);
-        // false is default
-        beanDumper = new JavaBeanDumper(false);
-        String output2 = beanDumper.dump(house);
-        assertEquals(etalon, output2);
-        // load
-        JavaBeanLoader<House> beanLoader = new JavaBeanLoader<House>(House.class);
-        House loadedHouse = beanLoader.load(yaml);
-        assertNotNull(loadedHouse);
-        assertEquals("Wall Street", loadedHouse.getStreet());
         // dump again
         String yaml3 = beanDumper.dump(loadedHouse);
         assertEquals(yaml, yaml3);
