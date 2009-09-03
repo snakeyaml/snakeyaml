337d4e7bc52837517ab029ac43480f2828ce2c59
somov <none@none>
2009-09-03 10:29:20 +0200

    use LinkedList in the example to deviate from default ArrayList

--- src/test/java/examples/CustomListExampleTest.java
+++ src/test/java/examples/CustomListExampleTest.java
@@ -16,7 +16,7 @@
 package examples;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import junit.framework.TestCase;
@@ -31,13 +31,13 @@ public class CustomListExampleTest extends TestCase {
         Loader loader = new Loader(new CustomConstructor());
         Yaml yaml = new Yaml(loader);
         List<Integer> data = (List<Integer>) yaml.load("[1, 2, 3]");
-        assertTrue(data instanceof ArrayList);
+        assertTrue(data instanceof LinkedList);
     }
 
     class CustomConstructor extends Constructor {
         @Override
         protected List<Object> createDefaultList(int initSize) {
-            return new ArrayList<Object>(initSize);
+            return new LinkedList<Object>();
         }
     }
 }
