d0c8f4b23eabdc9130164d779f84cd41b0dd90ce
somov <none@none>
2009-10-14 11:16:46 +0200

    org. imports in test

--- src/test/java/org/pyyaml/PyTokensTest.java
+++ src/test/java/org/pyyaml/PyTokensTest.java
@@ -20,7 +20,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
