ec2019f7fbf585207dd41431005eb57ce341abf0
py4fun <none@none>
2010-02-15 22:47:44 +0100

    add test for issue 52: check format for double quoted scalar

---
+++ src/test/java/org/yaml/snakeyaml/issues/issue52/LineBreakDooubleQuotedTest.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2008-2010 Andrey Somov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.yaml.snakeyaml.issues.issue52;
+
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.DumperOptions.ScalarStyle;
+
+/**
+ * @see <a
+ *      href="http://code.google.com/p/snakeyaml/issues/detail?id=52">Issue</a>
+ */
+public class LineBreakDooubleQuotedTest extends TestCase {
+    public void testDoubleQuotedStyle() throws IOException {
+        DumperOptions options = new DumperOptions();
+        options.setDefaultScalarStyle(ScalarStyle.DOUBLE_QUOTED);
+        options.setWidth(20);
+        options.setIndent(4);
+        Yaml yaml = new Yaml(options);
+        String etalon = "12345678901234567890\n\n123  456";
+        String output = yaml.dump(etalon);
+        System.out.println(output);
+        assertEquals("\"12345678901234567890\\n\\\n    \\n123  456\"\n", output);
+        String parsed = (String) yaml.load(output);
+        assertEquals(etalon, parsed);
+    }
+}
