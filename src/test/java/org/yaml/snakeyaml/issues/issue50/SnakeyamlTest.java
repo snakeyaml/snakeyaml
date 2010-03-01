cf3d2fdfc2d13c65b88f80acba6d3212bcd6f4b9
py4fun <none@none>
2010-03-01 22:04:34 +0100

    Issue 50 fails in Eclipse but works with Maven

---
+++ src/test/java/org/yaml/snakeyaml/issues/issue50/SnakeyamlTest.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2008-2010 Andrey Somov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.yaml.snakeyaml.issues.issue50;
+
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.yaml.snakeyaml.Yaml;
+
+/**
+ * test issue 50. XStream can get the JavaBean properties.
+ */
+public class SnakeyamlTest extends TestCase {
+    public static interface SomeBean {
+        String getAttribute1();
+
+        String getAttribute2();
+    }
+
+    /* public */static abstract class BaseSomeBean implements SomeBean {
+        private String attribute1;
+
+        public String getAttribute1() {
+            return attribute1;
+        }
+
+        public void setAttribute1(String attribute1) {
+            this.attribute1 = attribute1;
+        }
+    }
+
+    public static final class SomeBeanImpl extends BaseSomeBean {
+        private String attribute2;
+
+        public SomeBeanImpl(final String attribute1, final String attribute2) {
+            setAttribute1(attribute1);
+            setAttribute2(attribute2);
+        }
+
+        public String getAttribute2() {
+            return attribute2;
+        }
+
+        public void setAttribute2(String attribute2) {
+            this.attribute2 = attribute2;
+        }
+
+        @Override
+        public String toString() {
+            return "SomeBeanImpl";
+        }
+    }
+
+    public void test1() throws IOException {
+        SomeBean someBean = new SomeBeanImpl("value1", "value2");
+        Yaml dumper = new Yaml();
+        // final JavaBeanDumper dumper = new JavaBeanDumper();
+        String output = dumper.dump(someBean);
+        System.out.println(output);
+        assertEquals(
+                "!!org.yaml.snakeyaml.issues.issue50.SnakeyamlTest$SomeBeanImpl {attribute1: value1,\n  attribute2: value2}\n",
+                output);
+    }
+}
