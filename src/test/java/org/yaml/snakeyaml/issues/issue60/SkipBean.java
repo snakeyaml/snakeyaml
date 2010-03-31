2419b06b28ac72119b83c12144a81c6889aea092
py4fun <none@none>
2010-03-31 23:46:28 +0200

    add an example for issue 60: skip JavaBean properties

---
+++ src/test/java/org/yaml/snakeyaml/issues/issue60/SkipBean.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2008-2010 Andrey Somov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.yaml.snakeyaml.issues.issue60;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SkipBean {
+
+    private List<Integer> listInt;
+    private List<String> listStr;
+    private List<Date> listDate;
+    private List<File> empty = new ArrayList<File>(0);
+    private Map<String, Integer> map = new HashMap<String, Integer>(0);
+    private String text;
+    private Integer number;
+
+    public List<Integer> getListInt() {
+        return listInt;
+    }
+
+    public void setListInt(List<Integer> listInt) {
+        this.listInt = listInt;
+    }
+
+    public List<String> getListStr() {
+        return listStr;
+    }
+
+    public void setListStr(List<String> listStr) {
+        this.listStr = listStr;
+    }
+
+    public List<Date> getListDate() {
+        return listDate;
+    }
+
+    public void setListDate(List<Date> listDate) {
+        this.listDate = listDate;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public List<File> getEmpty() {
+        return empty;
+    }
+
+    public void setEmpty(List<File> empty) {
+        this.empty = empty;
+    }
+
+    public Map<String, Integer> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, Integer> map) {
+        this.map = map;
+    }
+}
---
+++ src/test/java/org/yaml/snakeyaml/issues/issue60/SkipBeanTest.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2008-2010 Andrey Somov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.yaml.snakeyaml.issues.issue60;
+
+import java.util.Arrays;
+
+import junit.framework.TestCase;
+
+import org.yaml.snakeyaml.Dumper;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Util;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.introspector.Property;
+import org.yaml.snakeyaml.nodes.CollectionNode;
+import org.yaml.snakeyaml.nodes.MappingNode;
+import org.yaml.snakeyaml.nodes.Node;
+import org.yaml.snakeyaml.nodes.NodeTuple;
+import org.yaml.snakeyaml.nodes.SequenceNode;
+import org.yaml.snakeyaml.nodes.Tag;
+import org.yaml.snakeyaml.representer.Representer;
+
+public class SkipBeanTest extends TestCase {
+
+    public void testSkipNull() {
+        Yaml yaml = new Yaml(new Dumper(new SkipNullRepresenter(), new DumperOptions()));
+        String output = yaml.dump(getBean());
+        // System.out.println(output);
+        assertEquals(Util.getLocalResource("issues/issue60-1.yaml"), output);
+    }
+
+    private class SkipNullRepresenter extends Representer {
+        @Override
+        protected NodeTuple representJavaBeanProperty(Object javaBean, Property property,
+                Object propertyValue, Tag customTag) {
+            if (propertyValue == null) {
+                return null;
+            } else {
+                return super
+                        .representJavaBeanProperty(javaBean, property, propertyValue, customTag);
+            }
+        }
+    }
+
+    public void testSkipEmptyCollections() {
+        Yaml yaml = new Yaml(new Dumper(new SkipEmptyRepresenter(), new DumperOptions()));
+        String output = yaml.dump(getBean());
+        // System.out.println(output);
+        assertEquals(Util.getLocalResource("issues/issue60-2.yaml"), output);
+    }
+
+    private class SkipEmptyRepresenter extends Representer {
+        @Override
+        protected NodeTuple representJavaBeanProperty(Object javaBean, Property property,
+                Object propertyValue, Tag customTag) {
+            NodeTuple tuple = super.representJavaBeanProperty(javaBean, property, propertyValue,
+                    customTag);
+            Node valueNode = tuple.getValueNode();
+            if (Tag.NULL.equals(valueNode.getTag())) {
+                return null;// skip 'null' values
+            }
+            if (valueNode instanceof CollectionNode) {
+                if (Tag.SEQ.equals(valueNode.getTag())) {
+                    SequenceNode seq = (SequenceNode) valueNode;
+                    if (seq.getValue().isEmpty()) {
+                        return null;// skip empty lists
+                    }
+                }
+                if (Tag.MAP.equals(valueNode.getTag())) {
+                    MappingNode seq = (MappingNode) valueNode;
+                    if (seq.getValue().isEmpty()) {
+                        return null;// skip empty maps
+                    }
+                }
+            }
+            return tuple;
+        }
+    }
+
+    private SkipBean getBean() {
+        SkipBean bean = new SkipBean();
+        bean.setText("foo");
+        bean.setListDate(null);
+        bean.setListInt(Arrays.asList(new Integer[] { null, 1, 2, 3 }));
+        bean.setListStr(Arrays.asList(new String[] { "bar", null, "foo", null }));
+        return bean;
+    }
+}
