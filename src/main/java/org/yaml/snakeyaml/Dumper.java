e41eda56503653ea147fe6fd86d0f22452b19ca1
py4fun <none@none>
2010-08-08 22:31:20 +0200

    Remove code from Loader and Dumper

--- src/main/java/org/yaml/snakeyaml/Dumper.java
+++ src/main/java/org/yaml/snakeyaml/Dumper.java
@@ -16,29 +16,17 @@
 
 package org.yaml.snakeyaml;
 
-import java.io.Writer;
-import java.util.Iterator;
-
-import org.yaml.snakeyaml.emitter.Emitter;
-import org.yaml.snakeyaml.error.YAMLException;
 import org.yaml.snakeyaml.representer.Representer;
-import org.yaml.snakeyaml.resolver.Resolver;
-import org.yaml.snakeyaml.serializer.Serializer;
 
 /**
  * @deprecated Dumper's functionality was moved to Yaml
  */
-public class Dumper {
+public final class Dumper {
     protected final Representer representer;
     protected final DumperOptions options;
-    private boolean attached = false;
 
     public Dumper(Representer representer, DumperOptions options) {
         this.representer = representer;
-        representer.setDefaultFlowStyle(options.getDefaultFlowStyle());
-        representer.setDefaultScalarStyle(options.getDefaultScalarStyle());
-        representer.getPropertyUtils().setAllowReadOnlyProperties(
-                options.isAllowReadOnlyProperties());
         this.options = options;
     }
 
@@ -52,29 +40,5 @@ public class Dumper {
 
     public Dumper() {
         this(new Representer(), new DumperOptions());
-    }
-
-    public void dump(Iterator<? extends Object> iter, Writer output, Resolver resolver) {
-        Serializer s = new Serializer(new Emitter(output, options), resolver, options);
-        try {
-            s.open();
-            while (iter.hasNext()) {
-                representer.represent(s, iter.next());
-            }
-            s.close();
-        } catch (java.io.IOException e) {
-            throw new YAMLException(e);
-        }
-    }
-
-    /**
-     * Because Dumper is stateful it cannot be shared
-     */
-    void setAttached() {
-        if (!attached) {
-            attached = true;
-        } else {
-            throw new YAMLException("Dumper cannot be shared.");
-        }
     }
 }
