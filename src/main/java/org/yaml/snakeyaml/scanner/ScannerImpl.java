c43488a4f7db8e8a04288c78d615f49f6c76ab22
py4fun <none@none>
2011-01-24 22:11:58 +0100

    Remove unused import in Scanner

--- src/main/java/org/yaml/snakeyaml/scanner/ScannerImpl.java
+++ src/main/java/org/yaml/snakeyaml/scanner/ScannerImpl.java
@@ -24,7 +24,6 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.regex.Pattern;
 
 import org.yaml.snakeyaml.error.Mark;
