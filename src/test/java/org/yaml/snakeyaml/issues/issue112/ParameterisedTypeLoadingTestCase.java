9dd1bad39970329ff057c588088143a5ac1adc7a
py4fun <none@none>
2011-05-25 19:07:29 +0200

    Fix issue 112: Improved support for parametarized types in collections

---
+++ src/test/java/org/yaml/snakeyaml/issues/issue112/ParameterisedTypeLoadingTestCase.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2008-2011, http://www.snakeyaml.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.yaml.snakeyaml.issues.issue112;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.yaml.snakeyaml.Yaml;
+
+public class ParameterisedTypeLoadingTestCase {
+
+    @Test
+    public void testParameterisedTypeLoading() throws IOException {
+        Yaml yamlParser = new Yaml(new MyYAMLConstructor(MyCompositeObject.class));
+
+        MyCompositeObject obj = (MyCompositeObject) yamlParser.load(this.getClass()
+                .getClassLoader().getResource("issues/issue112-1.yaml").openStream());
+
+        assertNotNull(obj);
+
+        for (Object thing : obj.getThings()) {
+            assertEquals(MyClass.class, thing.getClass());
+        }
+    }
+}
