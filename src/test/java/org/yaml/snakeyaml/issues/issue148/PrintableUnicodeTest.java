40504868066acc86406e53d38f7dc814bc914e4c
py4fun <none@none>
2012-06-07 22:12:38 +0200

    Add tests for issue 148

--- src/test/java/org/yaml/snakeyaml/issues/issue148/PrintableUnicodeTest.java
+++ src/test/java/org/yaml/snakeyaml/issues/issue148/PrintableUnicodeTest.java
@@ -13,11 +13,101 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.yaml.snakeyaml.emitter;
+package org.yaml.snakeyaml.issues.issue148;
 
 import java.util.Formatter;
 
-public class YamlCharacterEncoding {
+import junit.framework.TestCase;
+
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.DumperOptions.ScalarStyle;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.reader.ReaderException;
+
+public class PrintableUnicodeTest extends TestCase {
+    public void testFFFD() {
+        Yaml yaml = createYaml();
+        String fffd = yaml.dump("\uFFFD");
+        assertEquals("\"\\ufffd\"\n", fffd);
+    }
+
+    public void testSerialization() {
+        // test serialization of all Unicode codepoints
+        Yaml yaml = createYaml();
+        for (int c = Character.MIN_VALUE; c <= Character.MAX_VALUE; c++) {
+            String original = Character.toString((char) c);
+            String serialized = yaml.dump(original);
+
+            // "On output, a YAML processor must only produce these acceptable
+            // characters,
+            // and should also escape all non-printable Unicode characters."
+            for (int i = 0; i < serialized.length(); i++) {
+                int cp = (int) serialized.charAt(i);
+                if (!isAcceptable(cp))
+                    fail(String.format(
+                            "U+%04x: Serialization produced result with unacceptable U+%04x\n", c,
+                            cp));
+                if (!isPrintable(cp))
+                    fail(String.format(
+                            "U+%04x: Serialization produced result with nonprintable U+%04x\n", c,
+                            cp));
+            }
+        }
+    }
+
+    public void testDeserialization() {
+        // test deserialization of non-escaped codepoints
+        for (int c = Character.MIN_VALUE; c <= Character.MAX_VALUE; c++) {
+            // ignore breaks, which have special meaning
+            if (c == 0x0A || c == 0x0D || c == 0x85 || c == 0x2028 || c == 0x2029)
+                continue;
+            if (!isAcceptable(c) || c == 0x27)
+                continue;
+            String expected = Character.toString((char) c);
+            String serialized = "'" + expected + "'";
+
+            String result;
+            try {
+                result = new Yaml().load(serialized).toString();
+            } catch (ReaderException e) {
+                fail(String
+                        .format("U+%04x: Deserialization threw ReaderException for an acceptable character\n",
+                                c));
+                continue;
+            }
+            if (!result.equals(expected))
+                fail(String.format("U+%04x: Deserialization incorrect: %s\n", c, hexdump(result)));
+        }
+    }
+
+    public void testDeserialization2() {
+        // test deserialization of escaped codepoints
+        // "Any such characters must be presented using escape sequences."
+        for (int c = Character.MIN_VALUE; c <= Character.MAX_VALUE; c++) {
+            String expected = Character.toString((char) c);
+            String serialized = String.format("\"\\u%04x\"", c);
+
+            String result;
+            try {
+                result = new Yaml().load(serialized).toString();
+            } catch (ReaderException e) {
+                fail(String
+                        .format("U+%04x: Deserialization threw ReaderException for an acceptable escaped character\n",
+                                c));
+                continue;
+            }
+            if (!result.equals(expected))
+                fail(String.format("U+%04x: Deserialization of escaped character incorrect: %s\n",
+                        c, hexdump(result)));
+        }
+    }
+
+    private Yaml createYaml() {
+        DumperOptions options = new DumperOptions();
+        options.setAllowUnicode(false);
+        options.setDefaultScalarStyle(ScalarStyle.DOUBLE_QUOTED);
+        return new Yaml(options);
+    }
 
     /**
      * Test whether a character is printable, according to the YAML spec.
