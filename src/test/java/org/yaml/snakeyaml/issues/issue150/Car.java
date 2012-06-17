a18694591f1f69e1d45b1d6af3c80ebcc46c34a7
py4fun <none@none>
2012-06-17 22:48:19 +0200

    Add a test for issue 150

---
+++ src/test/java/org/yaml/snakeyaml/issues/issue150/Car.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2008-2012, http://www.snakeyaml.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.yaml.snakeyaml.issues.issue150;
+
+import java.util.Collection;
+
+/**
+ * 
+ * @author tturki
+ * 
+ */
+public class Car {
+    private String plate;
+    private Collection<Wheel> wheels;
+
+    public Car() {
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public Collection<Wheel> getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(Collection<Wheel> wheels) {
+        this.wheels = wheels;
+    }
+}
