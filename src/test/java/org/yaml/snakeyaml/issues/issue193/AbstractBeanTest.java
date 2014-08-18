6da3055614d5128a5c049d1b7e4707eda2e9c5ed
maslovalex <none@none>
2014-08-18 11:29:17 +0300

    some changes to test for issue 193

--- src/test/java/org/yaml/snakeyaml/issues/issue193/AbstractBeanTest.java
+++ src/test/java/org/yaml/snakeyaml/issues/issue193/AbstractBeanTest.java
@@ -16,7 +16,14 @@
 package org.yaml.snakeyaml.issues.issue193;
 
 import junit.framework.TestCase;
+
+import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.BaseConstructor;
+import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.introspector.BeanAccess;
+import org.yaml.snakeyaml.introspector.PropertyUtils;
+import org.yaml.snakeyaml.representer.Representer;
 
 public class AbstractBeanTest extends TestCase {
 
@@ -25,7 +32,18 @@ public class AbstractBeanTest extends TestCase {
         BeanA1 b = new BeanA1();
         b.setId(2l);
         b.setName("name1");
-        Yaml yaml = new Yaml();
+
+        Constructor c = new Constructor();
+        Representer r = new Representer();
+
+        PropertyUtils pu = new PropertyUtils();
+        c.setPropertyUtils(pu);
+        r.setPropertyUtils(pu);
+
+        pu.getProperties(BeanA1.class, BeanAccess.FIELD);
+
+        Yaml yaml = new Yaml(c, r);
+        // yaml.setBeanAccess(BeanAccess.FIELD);
         String dump = yaml.dump(b);
         BeanA1 b2 = (BeanA1) yaml.load(dump);
         assertEquals(b.getId(), b2.getId());
