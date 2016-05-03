c0fddb83c8a12308459a3792d056cb7b97099879
Andrey Somov <public.somov@gmail.com>
2016-05-03 17:16:47 +0200

    Fix issue 341

---
+++ src/test/java/org/yaml/snakeyaml/issues/issue341/MultiRepresenter.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2008, http://www.snakeyaml.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.yaml.snakeyaml.issues.issue341;
+
+import org.yaml.snakeyaml.nodes.Node;
+import org.yaml.snakeyaml.representer.Representer;
+
+class MultiRepresenter extends Representer {
+    public MultiRepresenter() {
+        multiRepresenters.put(null, new RepresentAsSring());
+    }
+
+    private class RepresentAsSring extends RepresentString {
+
+        public Node representData(Object data) {
+            return super.representData(data.toString());
+        }
+    }
+}
