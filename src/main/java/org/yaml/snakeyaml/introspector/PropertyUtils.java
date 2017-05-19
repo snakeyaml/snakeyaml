a8d6de8c570404cfe97f742deee12725190fe03a
Stephen Barker <mercurial@stephenjbarker.co.uk>
2017-05-19 20:52:36 +0100

    Add check into PropertyUtils for the Android Runtime and, if so, only get properties using Field access rather than with java.beans facilities, as java.beans isn't available on Android

--- src/main/java/org/yaml/snakeyaml/introspector/PropertyUtils.java
+++ src/main/java/org/yaml/snakeyaml/introspector/PropertyUtils.java
@@ -29,6 +29,7 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import org.yaml.snakeyaml.error.YAMLException;
+import org.yaml.snakeyaml.util.PlatformFeatureDetector;
 
 public class PropertyUtils {
 
@@ -37,6 +38,24 @@ public class PropertyUtils {
     private BeanAccess beanAccess = BeanAccess.DEFAULT;
     private boolean allowReadOnlyProperties = false;
     private boolean skipMissingProperties = false;
+
+    private PlatformFeatureDetector platformFeatureDetector;
+
+    public PropertyUtils() {
+        this(new PlatformFeatureDetector());
+    }
+
+    PropertyUtils(PlatformFeatureDetector platformFeatureDetector) {
+        this.platformFeatureDetector = platformFeatureDetector;
+
+        /*
+         * Android lacks much of java.beans (including the Introspector class, used here), because java.beans classes tend to rely on java.awt, which isn't
+         * supported in the Android SDK. That means we have to fall back on FIELD access only when SnakeYAML is running on the Android Runtime.
+         */
+        if (platformFeatureDetector.isRunningOnAndroid()) {
+            beanAccess = BeanAccess.FIELD;
+        }
+    }
 
     protected Map<String, Property> getPropertiesMap(Class<?> type, BeanAccess bAccess)
             throws IntrospectionException {
@@ -47,41 +66,41 @@ public class PropertyUtils {
         Map<String, Property> properties = new LinkedHashMap<String, Property>();
         boolean inaccessableFieldsExist = false;
         switch (bAccess) {
-        case FIELD:
-            for (Class<?> c = type; c != null; c = c.getSuperclass()) {
-                for (Field field : c.getDeclaredFields()) {
-                    int modifiers = field.getModifiers();
-                    if (!Modifier.isStatic(modifiers) && !Modifier.isTransient(modifiers)
-                            && !properties.containsKey(field.getName())) {
-                        properties.put(field.getName(), new FieldProperty(field));
-                    }
-                }
-            }
-            break;
-        default:
-            // add JavaBean properties
-            for (PropertyDescriptor property : Introspector.getBeanInfo(type)
-                    .getPropertyDescriptors()) {
-                Method readMethod = property.getReadMethod();
-                if (readMethod == null || !readMethod.getName().equals("getClass")) {
-                    properties.put(property.getName(), new MethodProperty(property));
-                }
-            }
-
-            // add public fields
-            for (Class<?> c = type; c != null; c = c.getSuperclass()) {
-                for (Field field : c.getDeclaredFields()) {
-                    int modifiers = field.getModifiers();
-                    if (!Modifier.isStatic(modifiers) && !Modifier.isTransient(modifiers)) {
-                        if (Modifier.isPublic(modifiers)) {
+            case FIELD:
+                for (Class<?> c = type; c != null; c = c.getSuperclass()) {
+                    for (Field field : c.getDeclaredFields()) {
+                        int modifiers = field.getModifiers();
+                        if (!Modifier.isStatic(modifiers) && !Modifier.isTransient(modifiers)
+                                && !properties.containsKey(field.getName())) {
                             properties.put(field.getName(), new FieldProperty(field));
-                        } else {
-                            inaccessableFieldsExist = true;
                         }
                     }
                 }
-            }
-            break;
+                break;
+            default:
+                // add JavaBean properties
+                for (PropertyDescriptor property : Introspector.getBeanInfo(type)
+                        .getPropertyDescriptors()) {
+                    Method readMethod = property.getReadMethod();
+                    if (readMethod == null || !readMethod.getName().equals("getClass")) {
+                        properties.put(property.getName(), new MethodProperty(property));
+                    }
+                }
+
+                // add public fields
+                for (Class<?> c = type; c != null; c = c.getSuperclass()) {
+                    for (Field field : c.getDeclaredFields()) {
+                        int modifiers = field.getModifiers();
+                        if (!Modifier.isStatic(modifiers) && !Modifier.isTransient(modifiers)) {
+                            if (Modifier.isPublic(modifiers)) {
+                                properties.put(field.getName(), new FieldProperty(field));
+                            } else {
+                                inaccessableFieldsExist = true;
+                            }
+                        }
+                    }
+                }
+                break;
         }
         if (properties.isEmpty() && inaccessableFieldsExist) {
             throw new YAMLException("No JavaBean properties found in " + type.getName());
@@ -136,6 +155,10 @@ public class PropertyUtils {
     }
 
     public void setBeanAccess(BeanAccess beanAccess) {
+        if (platformFeatureDetector.isRunningOnAndroid() && beanAccess != BeanAccess.FIELD) {
+            throw new IllegalArgumentException("JVM is Android - only BeanAccess.FIELD is available");
+        }
+
         if (this.beanAccess != beanAccess) {
             this.beanAccess = beanAccess;
             propertiesCache.clear();
@@ -153,7 +176,7 @@ public class PropertyUtils {
     /**
      * Skip properties that are missing during deserialization of YAML to a Java
      * object. The default is false.
-     * 
+     *
      * @param skipMissingProperties
      *            true if missing properties should be skipped, false otherwise.
      */
