a3909fc6de7aa245e2ea887da9eeb4ad545bc140
Andrey Somov <public.somov@gmail.com>
2018-02-24 18:39:03 +0100

    Minor refactoring in test - use enums, not classes

--- src/test/java/org/pyyaml/PyTokensTest.java
+++ src/test/java/org/pyyaml/PyTokensTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (c) 2008, http://www.snakeyaml.org
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,41 +15,19 @@
  */
 package org.pyyaml;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.yaml.snakeyaml.reader.StreamReader;
 import org.yaml.snakeyaml.reader.UnicodeReader;
 import org.yaml.snakeyaml.scanner.Scanner;
 import org.yaml.snakeyaml.scanner.ScannerImpl;
-import org.yaml.snakeyaml.tokens.AliasToken;
-import org.yaml.snakeyaml.tokens.AnchorToken;
-import org.yaml.snakeyaml.tokens.BlockEndToken;
-import org.yaml.snakeyaml.tokens.BlockEntryToken;
-import org.yaml.snakeyaml.tokens.BlockMappingStartToken;
-import org.yaml.snakeyaml.tokens.BlockSequenceStartToken;
-import org.yaml.snakeyaml.tokens.DirectiveToken;
-import org.yaml.snakeyaml.tokens.DocumentEndToken;
-import org.yaml.snakeyaml.tokens.DocumentStartToken;
-import org.yaml.snakeyaml.tokens.FlowEntryToken;
-import org.yaml.snakeyaml.tokens.FlowMappingEndToken;
-import org.yaml.snakeyaml.tokens.FlowMappingStartToken;
-import org.yaml.snakeyaml.tokens.FlowSequenceEndToken;
-import org.yaml.snakeyaml.tokens.FlowSequenceStartToken;
-import org.yaml.snakeyaml.tokens.KeyToken;
-import org.yaml.snakeyaml.tokens.ScalarToken;
 import org.yaml.snakeyaml.tokens.StreamEndToken;
 import org.yaml.snakeyaml.tokens.StreamStartToken;
-import org.yaml.snakeyaml.tokens.TagToken;
 import org.yaml.snakeyaml.tokens.Token;
-import org.yaml.snakeyaml.tokens.ValueToken;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * imported from PyYAML
@@ -57,25 +35,25 @@ import org.yaml.snakeyaml.tokens.ValueToken;
 public class PyTokensTest extends PyImportTest {
 
     public void testTokens() throws FileNotFoundException {
-        Map<Class<?>, String> replaces = new HashMap<Class<?>, String>();
-        replaces.put(DirectiveToken.class, "%");
-        replaces.put(DocumentStartToken.class, "---");
-        replaces.put(DocumentEndToken.class, "...");
-        replaces.put(AliasToken.class, "*");
-        replaces.put(AnchorToken.class, "&");
-        replaces.put(TagToken.class, "!");
-        replaces.put(ScalarToken.class, "_");
-        replaces.put(BlockSequenceStartToken.class, "[[");
-        replaces.put(BlockMappingStartToken.class, "{{");
-        replaces.put(BlockEndToken.class, "]}");
-        replaces.put(FlowSequenceStartToken.class, "[");
-        replaces.put(FlowSequenceEndToken.class, "]");
-        replaces.put(FlowMappingStartToken.class, "{");
-        replaces.put(FlowMappingEndToken.class, "}");
-        replaces.put(BlockEntryToken.class, ",");
-        replaces.put(FlowEntryToken.class, ",");
-        replaces.put(KeyToken.class, "?");
-        replaces.put(ValueToken.class, ":");
+        Map<Token.ID, String> replaces = new HashMap<Token.ID, String>();
+        replaces.put(Token.ID.Directive, "%");
+        replaces.put(Token.ID.DocumentStart, "---");
+        replaces.put(Token.ID.DocumentEnd, "...");
+        replaces.put(Token.ID.Alias, "*");
+        replaces.put(Token.ID.Anchor, "&");
+        replaces.put(Token.ID.Tag, "!");
+        replaces.put(Token.ID.Scalar, "_");
+        replaces.put(Token.ID.BlockSequenceStart, "[[");
+        replaces.put(Token.ID.BlockMappingStart, "{{");
+        replaces.put(Token.ID.BlockEnd, "]}");
+        replaces.put(Token.ID.FlowSequenceStart, "[");
+        replaces.put(Token.ID.FlowSequenceEnd, "]");
+        replaces.put(Token.ID.FlowMappingStart, "{");
+        replaces.put(Token.ID.FlowMappingEnd, "}");
+        replaces.put(Token.ID.BlockEntry, ",");
+        replaces.put(Token.ID.FlowEntry, ",");
+        replaces.put(Token.ID.Key, "?");
+        replaces.put(Token.ID.Value, ":");
         //
         File[] tokensFiles = getStreamsByExtension(".tokens");
         assertTrue("No test files found.", tokensFiles.length > 0);
@@ -99,7 +77,7 @@ public class PyTokensTest extends PyImportTest {
                 while (scanner.checkToken(new Token.ID[0])) {
                     Token token = scanner.getToken();
                     if (!(token instanceof StreamStartToken || token instanceof StreamEndToken)) {
-                        String replacement = replaces.get(token.getClass());
+                        String replacement = replaces.get(token.getTokenId());
                         tokens1.add(replacement);
                     }
                 }
