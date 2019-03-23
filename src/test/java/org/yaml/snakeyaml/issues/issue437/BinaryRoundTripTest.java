89232f77ec21d75baf2c0e77bf1a786e2e07d41c
Andrey Somov <public.somov@gmail.com>
2019-03-23 14:13:30 +0100

    Improve extended test for issue 437

--- src/test/java/org/yaml/snakeyaml/issues/issue437/BinaryRoundTripTest.java
+++ src/test/java/org/yaml/snakeyaml/issues/issue437/BinaryRoundTripTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (c) 2008, http://www.snakeyaml.org
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -45,11 +45,12 @@ public class BinaryRoundTripTest extends TestCase {
         Yaml underTest = new Yaml();
         String source = "\u0096";
         Node node = underTest.represent(source);
-
+        //check Node
         assertEquals(Tag.BINARY, node.getTag());
         assertEquals(NodeId.scalar, node.getNodeId());
         ScalarNode scalar = (ScalarNode) node;
         assertEquals("wpY=", scalar.getValue());
+        //check Event
         List<Event> events = underTest.serialize(node);
         assertEquals(5, events.size());
         ScalarEvent data = (ScalarEvent) events.get(2);
