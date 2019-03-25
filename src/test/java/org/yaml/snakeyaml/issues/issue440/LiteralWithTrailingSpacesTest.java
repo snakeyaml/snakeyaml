5ed9eab0b67da6cd2c36adef8a6e756d31bd739e
Andrey Somov <public.somov@gmail.com>
2019-03-25 21:28:06 +0100

    Add test for issue 440

---
+++ src/test/java/org/yaml/snakeyaml/issues/issue440/LiteralWithTrailingSpacesTest.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2008, http://www.snakeyaml.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.yaml.snakeyaml.issues.issue440;
+
+import junit.framework.TestCase;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
+
+public class LiteralWithTrailingSpacesTest extends TestCase {
+
+    public void testLiteral() {
+        DumperOptions options = new DumperOptions();
+        options.setDefaultScalarStyle(DumperOptions.ScalarStyle.LITERAL);
+        Yaml yaml = new Yaml(options);
+        String correct = "this is some text with \"quotes\" and\nline breaks";
+        String correctYaml = yaml.dump(correct);
+        assertEquals("|-\n" +
+                "  this is some text with \"quotes\" and\n" +
+                "  line breaks\n", correctYaml);
+    }
+
+    public void testTrimTrailingSpace() {
+        Yaml yaml = new Yaml();
+        String parsed = yaml.load("trailing ");
+        assertEquals("The trailing space must be removed.", "trailing", parsed);
+    }
+
+    public void testLiteralWithNewLine() {
+        DumperOptions options = new DumperOptions();
+        options.setDefaultScalarStyle(DumperOptions.ScalarStyle.LITERAL);
+        Yaml yaml = new Yaml(options);
+        String inCorrect = "this is some text with \"quotes\" and \nline breaks";
+        String inCorrectYaml = yaml.dump(inCorrect);
+        System.out.println(inCorrectYaml);
+        assertEquals("\"this is some text with \\\"quotes\\\" and \\nline breaks\"\n", inCorrectYaml);
+        //TODO assertTrue(inCorrectYaml.contains("|"));
+    }
+}
