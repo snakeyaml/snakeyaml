d41964533cb87fb3c01f7ca5b3e397375fa34e7f
treekt <kSncM6vFd6&eGstDk!a6>
2021-01-07 15:26:11 +0100

    Fixed java doc and generic enum type

--- src/main/java/org/yaml/snakeyaml/util/EnumUtils.java
+++ src/main/java/org/yaml/snakeyaml/util/EnumUtils.java
@@ -4,12 +4,12 @@ public class EnumUtils {
 
     /**
      * Looks for an enumeration constant that matches the string without being case sensitive
-     * @param enumType the Class object of the enum type from which to return a constant
-     * @param name the name of the constant to return
-     * @param <T> the enum type whose constant is to be returned
+     * @param enumType - the Class object of the enum type from which to return a constant
+     * @param name - the name of the constant to return
+     * @param <T> - the enum type whose constant is to be returned
      * @return the enum constant of the specified enum type with the specified name, insensitive to case
      */
-    public static <T extends Enum<?>> T findEnumInsensitiveCase(Class<T> enumType, String name) {
+    public static <T extends Enum<T>> T findEnumInsensitiveCase(Class<T> enumType, String name) {
         for (T constant : enumType.getEnumConstants()) {
             if (constant.name().compareToIgnoreCase(name) == 0) {
                 return constant;
