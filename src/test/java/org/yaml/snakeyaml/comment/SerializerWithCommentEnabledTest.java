d863e6cc84742bbe264d2e331227df998c873417
Wolf2323 <gabrielpatrikurban@gmail.com>
2021-04-07 23:03:19 +0200

    disable debug

--- src/test/java/org/yaml/snakeyaml/comment/SerializerWithCommentEnabledTest.java
+++ src/test/java/org/yaml/snakeyaml/comment/SerializerWithCommentEnabledTest.java
@@ -37,7 +37,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class SerializerWithCommentEnabledTest {
-    private boolean DEBUG = true;
+    private boolean DEBUG = false;
 
     private void println(String s) {
         if (DEBUG) System.out.println(s);
