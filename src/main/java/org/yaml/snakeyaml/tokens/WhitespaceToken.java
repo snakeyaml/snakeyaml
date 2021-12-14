d76039c77b14fbc265e8de880a3b0b97872baebf
Andrey Somov <asomov@unifonic.com>
2021-12-14 10:08:59 +0400

    Remove snakeyaml.org

--- src/main/java/org/yaml/snakeyaml/tokens/WhitespaceToken.java
+++ src/main/java/org/yaml/snakeyaml/tokens/WhitespaceToken.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2008, http://www.snakeyaml.org
+ * Copyright (c) 2008, SnakeYAML
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
