b33f0ca7507fd61fc945722b5d14de4b0aff5262
Clayton Walker <clayton.m.walker@gmail.com>
2022-10-21 00:48:34 -0600

    Add module-info.java

---
+++ src/main/java9/module-info.java
@@ -0,0 +1,23 @@
+module org.yaml.snakeyaml {
+    requires java.logging;
+
+    exports org.yaml.snakeyaml;
+    exports org.yaml.snakeyaml.comments;
+    exports org.yaml.snakeyaml.composer;
+    exports org.yaml.snakeyaml.constructor;
+    exports org.yaml.snakeyaml.emitter;
+    exports org.yaml.snakeyaml.env;
+    exports org.yaml.snakeyaml.error;
+    exports org.yaml.snakeyaml.events;
+    exports org.yaml.snakeyaml.extensions.compactnotation;
+    exports org.yaml.snakeyaml.introspector;
+    exports org.yaml.snakeyaml.nodes;
+    exports org.yaml.snakeyaml.parser;
+    exports org.yaml.snakeyaml.reader;
+    exports org.yaml.snakeyaml.representer;
+    exports org.yaml.snakeyaml.resolver;
+    exports org.yaml.snakeyaml.scanner;
+    exports org.yaml.snakeyaml.serializer;
+    exports org.yaml.snakeyaml.tokens;
+    exports org.yaml.snakeyaml.util;
+}
