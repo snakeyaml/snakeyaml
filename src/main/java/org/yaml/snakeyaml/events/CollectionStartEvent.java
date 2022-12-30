2190f92af4cbe3507bd8c6f87bd6e7b0582f4318
Andrey Somov <public.somov@gmail.com>
2022-12-30 10:14:04 +0400

    Remove long deprecated methods in FlowStyle

--- src/main/java/org/yaml/snakeyaml/events/CollectionStartEvent.java
+++ src/main/java/org/yaml/snakeyaml/events/CollectionStartEvent.java
@@ -50,19 +50,6 @@ public abstract class CollectionStartEvent extends NodeEvent {
   }
 
   /**
-   * Existed in older versions but replaced with {@link DumperOptions.FlowStyle}-based constructor.
-   * Restored in v1.22 for backwards compatibility.
-   *
-   * @deprecated Since restored in v1.22. Use
-   *             {@link CollectionStartEvent#CollectionStartEvent(String, String, boolean, Mark, Mark, org.yaml.snakeyaml.DumperOptions.FlowStyle) }.
-   */
-  @Deprecated
-  public CollectionStartEvent(String anchor, String tag, boolean implicit, Mark startMark,
-      Mark endMark, Boolean flowStyle) {
-    this(anchor, tag, implicit, startMark, endMark, DumperOptions.FlowStyle.fromBoolean(flowStyle));
-  }
-
-  /**
    * Tag of this collection.
    *
    * @return The tag of this collection, or <code>null</code> if no explicit tag is available.
