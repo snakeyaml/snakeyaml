a488f0b40fb0d09a2b9c50fddbe0a818ffb9f9f3
Andrey Somov <public.somov@gmail.com>
2023-03-25 17:16:00 +0400

    Add a test

---
+++ src/test/java/org/yaml/snakeyaml/issues/issueNNN/FuzzYAMLRead56902Test.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2008, SnakeYAML
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.yaml.snakeyaml.issues.issueNNN;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.scanner.ScannerException;
+
+/**
+ * https://github.com/FasterXML/jackson-dataformats-text/issues/406
+ * https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=56902
+ */
+public class FuzzYAMLRead56902Test {
+
+  @Test
+  public void testIncompleteValue() {
+    Yaml yaml = new Yaml();
+    try {
+      yaml.load("%YAML 1.9224775801");
+      fail("Invalid escape code in double quoted scalar should not be accepted");
+    } catch (ScannerException e) {
+      assertTrue(e.getMessage(), e.getMessage()
+          .contains("found a number which cannot represent a valid version: 9224775801"));
+    }
+  }
+}
