c4c7ba0a85e099d645543d018eaf074efb8bb265
Andrey Somov <public.somov@gmail.com>
2023-04-08 16:20:04 +0400

    Use issue 1061

--- src/test/java/org/yaml/snakeyaml/issues/issue1061/FuzzYAMLRead56902Test.java
+++ src/test/java/org/yaml/snakeyaml/issues/issue1061/FuzzYAMLRead56902Test.java
@@ -11,7 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.yaml.snakeyaml.issues.issueNNN;
+package org.yaml.snakeyaml.issues.issue1061;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
