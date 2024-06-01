f9c1c70a515468eec172cf6d89c256b3d0dbf0dc
Andrey Somov <public.somov@gmail.com>
2024-06-01 11:32:30 +0400

    Fix docker script

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+# it may be needed for older docker    -u `id -u`:`id -g`
+
 docker run --rm -it               \
-    -u `id -u`:`id -g`            \
     -v `pwd`:/work                \
     -v ~:/my-home                 \
     -e "HOME=/my-home"            \
