1430c40c746194248b1d69c37c28afb78c873a50
Andrey Somov <public.somov@gmail.com>
2024-06-12 10:17:32 +0400

    Improve test for issue 1091

--- src/test/java/org/yaml/snakeyaml/issues/issue1091/FileEncodingTest.java
+++ src/test/java/org/yaml/snakeyaml/issues/issue1091/FileEncodingTest.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2008, SnakeYAML
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -14,6 +14,8 @@
 package org.yaml.snakeyaml.issues.issue1091;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 import org.yaml.snakeyaml.Util;
@@ -26,14 +28,23 @@ import org.yaml.snakeyaml.Yaml;
 public class FileEncodingTest {
 
   @Test
-  public void testNoAnchorForScalar() {
+  public void testReadingFile() {
     Yaml yaml = new Yaml();
     String origin = Util.getLocalResource("issues/issue1091-input.yaml");
     Object parsed = yaml.load(origin);
-
     assertEquals(1, 1);
   }
 
-
+  @Test
+  public void testReadingFileAsString() {
+    Yaml yaml = new Yaml();
+    String origin = Util.getLocalResource("issues/issue1091-input.yaml");
+    try {
+      yaml.loadAs(origin, String.class);
+      fail();
+    } catch (Exception e) {
+      assertTrue(e.getMessage(), e.getMessage().contains("Cannot create property=heading"));
+    }
+  }
 }
 
